a505b922543af514e396df3546b236910b01c9e1
Jay Phillips <jicksta@gmail.com>
2016-01-24 16:16:02 -0800

    Fixing rvm

--- zsh/macosx.zsh
+++ zsh/macosx.zsh
@@ -1,1 +1,4 @@
 alias changehostname="sudo scutil --set HostName"
+
+[[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,32 @@
+
 autoload -U colors && colors
-autoload predict-on && predict-on
+#autoload predict-on && predict-on
+
+zmodload zsh/complist
+autoload -U compinit && compinit
+
+zstyle ':completion:::::' completer _complete _approximate
+
+zstyle ':completion:*:descriptions' format "- %d -"
+zstyle ':completion:*:corrections' format "- %d - (errors %e})"
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
+zstyle ':completion:*' menu select
+zstyle ':completion:*' verbose yes
+
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX + $#SUFFIX) / 3 )) )'
+zstyle ':completion::approximate*:*' prefix-needed false
+
+########################################################
 
 export PS1="%{$fg[green]%}%~ %{$fg[white]%}%%%  "
 
+########################################################
+
 #antigen theme bhilburn/powerlevel9k powerlevel9k
 #antigen apply
+
+
+
