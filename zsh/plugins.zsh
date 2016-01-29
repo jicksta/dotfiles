d5c92ed026a41dac774ad2eecb49e0fcc5d77906
Jay Phillips <jicksta@gmail.com>
2016-01-28 22:07:28 -0800

    Switching to powerlevel9k antigen zsh theme

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,4 +1,3 @@
-
 antigen bundle robbyrussell/oh-my-zsh plugins/ruby
 antigen bundle git
 antigen bundle kennethreitz/autoenv
@@ -7,8 +6,18 @@ antigen bundle command-not-found
 
 antigen use oh-my-zsh
 
-antigen theme robbyrussell
+antigen theme bhilburn/powerlevel9k powerlevel9k
+#antigen theme robbyrussell
 #antigen theme agnoster
-#antigen theme bhilburn/powerlevel9k powerlevel9k
+
+### More themes: https://github.com/robbyrussell/oh-my-zsh/wiki/themes
+
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status rbenv)
+POWERLEVEL9K_STATUS_VERBOSE=false
+POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+
 
 antigen apply
