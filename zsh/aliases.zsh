7a0ab30233d3c418c04abfa8e9cd26e2ebf78160
Jay Phillips <jicksta@gmail.com>
2016-05-30 15:22:01 -0700

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,21 +1,32 @@
+# Shell Utilities
 alias l="ls -GAChl"
 alias ls="ls -G"
-alias py="python -q"
+alias t="tree -AC"
+alias shred="gshred" # some coreutils programs are prefixed with a "g"
+
+# Git
+alias gc="git clone"
+
+# Languages
 alias b="bundle"
 alias be="bundle exec"
+alias py="python -q"
+
+# Ember
 alias em="ember"
 alias et="ember test"
 alias ets="ember test --server"
+
+# Containers
 alias d="docker"
 alias dc="docker-compose"
 alias dm="docker-machine"
-
-alias serve="python -m SimpleHTTPServer 1234"
-
 function dme() {
   eval "$(docker-machine env $1)"
 }
 
-# <zsh-quirks>
+# Webdev Utils
+alias serve="python -m SimpleHTTPServer 1234"
+
+# ZSH Quirks
 alias find="noglob find"
-# </zsh-quirks>
