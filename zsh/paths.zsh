c52ab176b6dcc7c7dcfe7a0b62162c537c19069f
Jay Phillips <jicksta@gmail.com>
2016-12-02 00:56:26 -0800

    Update zsh/paths.zsh

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,11 +1,12 @@
+export GOPATH="$HOME/code/gopath"
+
+# export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
+# export  JDK_HOME="$(/usr/libexec/java_home -v 1.7)"
+
+export PATH="$GOPATH/bin:$PATH"
 export PATH="$HOME/.yarn-config/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 export PATH="$PATH:$HOME/.zsh/bin"
 export PATH="$PATH:$HOME/.bin/"
-export PATH="$PATH:/usr/local/opt/go/libexec/bin"
 export PATH="/usr/local/sbin:$PATH" # For Homebrew
 
-export GOPATH="$HOME/code/gopath"
-
-#export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
-#export JDK_HOME="$(/usr/libexec/java_home -v 1.7)"
