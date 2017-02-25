8f7e6161893440d7b98ac1d69cd09d1c10c5b8be
Jay Phillips <jicksta@gmail.com>
2017-02-25 11:59:57 -0800

    update paths.zsh

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,12 +1,15 @@
 export GOPATH="$HOME/code/gopath"
 
-# export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
-# export  JDK_HOME="$(/usr/libexec/java_home -v 1.7)"
 
 export PATH="$GOPATH/bin:$PATH"
-export PATH="$HOME/.yarn-config/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
+export PATH="$PATH:/usr/local/Caskroom"
 export PATH="$PATH:$HOME/.zsh/bin"
 export PATH="$PATH:$HOME/.bin/"
 export PATH="/usr/local/sbin:$PATH" # For Homebrew
 
+# export PATH="/usr/local/yarn/bin:$PATH"
+# export PATH="$HOME/.yarn-config/global/node_modules/.bin:$PATH"
+
+# Makes `ssh -i ...` easier to type out for specific keys in ~/.ssh
+export SSH="~/.ssh"
