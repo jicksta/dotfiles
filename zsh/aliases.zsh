0c94f44e9b9a17cf580909793285eaa3a425f1fb
Jay Phillips <jicksta@gmail.com>
2017-08-05 15:18:21 -0400

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,19 @@
+# Better ls utilities: l, t, and e
+alias  l="ls -GAChl"
+>>>>>>> Update aliases.zsh
+if which exa > /dev/null; then
+  alias e="exa --git --long"
+  alias t="exa --git --tree"
+else
+  alias  e="echo Need to 'brew install exa'"
+  alias  t="tree -AC"
+fi
+
 # Shell Utilities
-alias     l="ls -GAChl"
 alias shred="gshred --remove" # some coreutils programs are prefixed with a "g"
 alias   tmp="mkdir -p /tmp/tmp && cd /tmp/tmp"
 alias  find="noglob find"
 alias     x="chmod +x"
-
-if which exa > /dev/null; then
-  ls() {
-    exa --git --long $@
-  }
-  alias  t="exa --tree"
-else
-  alias ls="ls -G"
-  alias  t="tree -AC"
-fi
 
 # Languages
 alias  b="bundle"
@@ -24,7 +24,6 @@ alias p3="python3"
 # Web Development utils
 alias     y="yarn"
 alias   run="yarn run"
-alias     n="npm"
 alias serve="python -m SimpleHTTPServer 1234"
 
 # Data Engineering utils
@@ -35,20 +34,15 @@ alias  em="ember"
 alias  et="ember test"
 alias ets="ember test --server"
 
-# Docker / Kubernetes
-alias   d="docker"
-alias  dc="docker-compose"
-alias  dm="docker-machine"
+# Docker
+alias  d="docker"
+alias dc="docker-compose"
+alias dm="docker-machine"
+dme() { eval "$(docker-machine env $1)" }
+
+# Kubernetes
 alias   k="kubectl"
 alias  kg="kubectl get"
-alias   m="minikube"
+alias  mk="minikube"
 alias msx="minikube start --vm-driver=xhyve"
-
-function mke() {
-  eval "$(minikube docker-env)"
-  export MINIKUBE_IP="$(minikube ip)"
-}
-
-function dme() {
-  eval "$(docker-machine env $1)"
-}
+mke() { eval "$(minikube docker-env)" }
